4e5905ee9ab9fdd1c06f5fc6206fe0f778a14da0
mariano quiroga <mariano.eq@gmail.com>
2023-03-13 17:47:36 +0100

    Add DB config and DBconnection function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import http from 'http';
+import createDebug from 'debug';
 import { app } from './app.js';
+import { dbConnection } from './db/db.connection.js';
 
-const port = process.env.PORT || 3000;
+const debug = createDebug('SERVER:index');
+const PORT = process.env.PORT || 3232;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+dbConnection()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('DB: ', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => {
+    server.emit('error' + error);
+  });
+
+server.on('listening', () => {
+  debug('Server listening on PORT ' + PORT);
+});
+
+server.on('error', (error) => {
+  debug('server Error ' + error);
 });
